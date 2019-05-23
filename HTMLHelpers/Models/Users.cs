05c84c86dc1fc3568cdd9ebc6072a64ca5343395
Faisal Pathan <faisalmpathan@gmail.com>
2019-05-23 16:59:07 +0530

    Initial Commit

---
+++ HTMLHelpers/Models/Users.cs
@@ -0,0 +1,15 @@
+﻿
+namespace HTMLHelpers.Models
+{
+    public class Users
+    {
+        public string Gender { get; set; }
+        public string Password { get; set; }
+        public string User { get; set; }
+        public bool Terms { get; set; }
+        public string Name { get; set; }
+        public string Phone { get; set; }
+        public string Email { get; set; }
+
+    }
+}
